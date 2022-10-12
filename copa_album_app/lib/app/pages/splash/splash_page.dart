8fda5613e578a8725e01e1e2c7c97e5e678ce07d
Gisele <gisesonia@gmail.com>
2022-10-12 13:26:27 -0300

    loader e mensagens de erro

--- copa_album_app/lib/app/pages/splash/splash_page.dart
+++ copa_album_app/lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:copa_album_app/app/core/ui/helpers/loader.dart';
+import 'package:copa_album_app/app/core/ui/helpers/messages.dart';
 import 'package:copa_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:copa_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:copa_album_app/app/core/ui/widgets/button.dart';
@@ -12,7 +13,8 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> with Loader {
+class _SplashPageState extends State<SplashPage>
+    with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,27 +27,33 @@ class _SplashPageState extends State<SplashPage> with Loader {
             ElevatedButton(
               onPressed: () async {
                 showLoader();
-                await Future.delayed(Duration(seconds: 2));
+                await Future.delayed(const Duration(seconds: 2));
                 hideLoader();
               },
               style: ButtonStyles.i.yellowButton,
               child: const Text('Salvar'),
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                showError('Erro no botão outline');
+              },
               style: ButtonStyles.i.primaryOutlineButton,
               child: const Text('Salvar'),
             ),
             const TextField(),
             Button(
-                onPressed: () {},
+                onPressed: () {
+                  showInfo('Info no botão outline');
+                },
                 style: ButtonStyles.i.primaryButton,
                 labelStyle: context.textStyles.textPrimaryFontBold,
                 label: 'Salvar'),
             Button.primary(
               width: MediaQuery.of(context).size.width * .9,
               height: 130,
-              onPressed: () {},
+              onPressed: () {
+                showSuccess('Success no botão');
+              },
               label: 'Salvar',
             ),
             RoundedButtton(icon: Icons.add, onPressed: (() {}))
