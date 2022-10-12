cc5f3ad7e1acbf0c62767ef60d47ca5bc92c4e40
Gisele <gisesonia@gmail.com>
2022-10-12 08:48:10 -0300

    exportando informações de tema para uma classe

--- copa_album_app/lib/app/pages/splash/splash_page.dart
+++ copa_album_app/lib/app/pages/splash/splash_page.dart
@@ -1,6 +1,4 @@
 import 'package:copa_album_app/app/core/styles/button_styles.dart';
-import 'package:copa_album_app/app/core/styles/colors_app.dart';
-import 'package:copa_album_app/app/core/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -8,60 +6,25 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var _defaultInputBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(18),
-      borderSide: BorderSide(color: ColorsApp.i.greyDart, width: 1),
-    );
-    return Theme(
-      data: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: const IconThemeData(
-            color: Colors.black,
-          ),
-          titleTextStyle: context.textStyles.textPrimaryFontBold.copyWith(
-            color: Colors.black,
-            fontSize: 18,
-          ),
-        ),
-        primaryColor: context.colors.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: context.colors.primary,
-          primary: context.colors.primary,
-          secondary: context.colors.secondary,
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-            fillColor: Colors.white,
-            filled: true,
-            isDense: true,
-            contentPadding: const EdgeInsets.all(14),
-            border: _defaultInputBorder,
-            enabledBorder: _defaultInputBorder,
-            focusedBorder: _defaultInputBorder,
-            labelStyle: context.textStyles.labelTextField),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen'),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Splash Screen'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyles.i.yellowButton,
-                child: const Text('Salvar'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                style: ButtonStyles.i.primaryOutlineButton,
-                child: const Text('Salvar'),
-              ),
-              const TextField(),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyles.i.yellowButton,
+              child: const Text('Salvar'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: ButtonStyles.i.primaryOutlineButton,
+              child: const Text('Salvar'),
+            ),
+            const TextField(),
+          ],
         ),
       ),
     );
